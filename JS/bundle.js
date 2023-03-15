9b1f2e81d2b169075a1d5dc2b5ffb5632597db1e
Irina Kosogor <i.v.kosogor@gmail.com>
2023-03-15 19:47:10 +0200

    add services animation

--- JS/bundle.js
+++ JS/bundle.js
@@ -361,10 +361,14 @@ const services = async () => {
 	const planningBtn = document.querySelector("#planningBtn");
 	const cardContainer = document.querySelector(".our-services-cards");
 
-    const design = await fetch("JS/json/design.json")
+	const design = await fetch("JS/json/design.json")
 		.then((response) => response.json())
 		.then((data) => {
-            return data.projects.sort((a, b) => new Date(b.created_at).getTime() -  new Date(a.created_at).getTime());
+			return data.projects.sort(
+				(a, b) =>
+					new Date(b.created_at).getTime() -
+					new Date(a.created_at).getTime()
+			);
 		})
 		.catch((error) => {
 			console.error(error);
@@ -373,7 +377,11 @@ const services = async () => {
 	const architecture = await fetch("JS/json/architecture.json")
 		.then((response) => response.json())
 		.then((data) => {
-            return data.projects.sort((a, b) => new Date(b.created_at).getTime() -  new Date(a.created_at).getTime());
+			return data.projects.sort(
+				(a, b) =>
+					new Date(b.created_at).getTime() -
+					new Date(a.created_at).getTime()
+			);
 		})
 		.catch((error) => {
 			console.error(error);
@@ -382,15 +390,19 @@ const services = async () => {
 	const planning = await fetch("JS/json/planning.json")
 		.then((response) => response.json())
 		.then((data) => {
-			return data.projects.sort((a, b) => new Date(b.created_at).getTime() -  new Date(a.created_at).getTime());
+			return data.projects.sort(
+				(a, b) =>
+					new Date(b.created_at).getTime() -
+					new Date(a.created_at).getTime()
+			);
 		})
 		.catch((error) => {
 			console.error(error);
 		});
 
-    const projects = [...design, ...architecture, ...planning];
+	const projects = [...design, ...architecture, ...planning];
 
-    handleFilterClick("all");
+	handleFilterClick("all");
 
 	function displayProjects(projects, n) {
 		cardContainer.innerHTML = "";
@@ -398,12 +410,12 @@ const services = async () => {
 			const project = projects[i];
 			const card = document.createElement("div");
 			card.classList.add("our-services-card");
-            if (project.category === "architecture") {
-                card.classList.add("our-services-card_architecture"); 
-            }
-            if (project.category === "planning") {
-                card.classList.add("our-services-card_planning"); 
-            }
+			if (project.category === "architecture") {
+				card.classList.add("our-services-card_architecture");
+			}
+			if (project.category === "planning") {
+				card.classList.add("our-services-card_planning");
+			}
 			card.innerHTML = `
                 <div class="our-services-card__title">${project.title}</div>
                 <div class="our-services-card__description">${project.description}</div>
@@ -412,46 +424,46 @@ const services = async () => {
 		}
 	}
 
-    function handleFilterClick(category) {
-        let filteredProjects;
-        if (category === "all") {
-            filteredProjects = [design[0], architecture[0], planning[0]];
-            displayProjects(filteredProjects, 3);
-        } else {
-            filteredProjects = projects.filter(
-                (project) => project.category === category
-            );
-            displayProjects(filteredProjects, 5);
-        }
+	function handleFilterClick(category) {
+		let filteredProjects;
+		if (category === "all") {
+			filteredProjects = [design[0], architecture[0], planning[0]];
+			displayProjects(filteredProjects, 3);
+		} else {
+			filteredProjects = projects.filter(
+				(project) => project.category === category
+			);
+			displayProjects(filteredProjects, 5);
+		}
 	}
 
-    function removeActiveClass() {
-        allBtn.classList.remove("active");
+	function removeActiveClass() {
+		allBtn.classList.remove("active");
 		interiorBtn.classList.remove("active");
 		architectureBtn.classList.remove("active");
 		planningBtn.classList.remove("active");
-    }
+	}
 
-    allBtn.addEventListener("click", () => {
-        removeActiveClass()
-		allBtn.classList.add("active");
+	allBtn.addEventListener("click", () => {
+		removeActiveClass();
+		allBtn.classList.toggle("active");
 		handleFilterClick("all");
 	});
 
 	interiorBtn.addEventListener("click", () => {
-        removeActiveClass()
+		removeActiveClass();
 		interiorBtn.classList.toggle("active");
 		handleFilterClick("interior-design");
 	});
-    
+
 	architectureBtn.addEventListener("click", () => {
-        removeActiveClass()
+		removeActiveClass();
 		architectureBtn.classList.toggle("active");
 		handleFilterClick("architecture");
 	});
-    
+
 	planningBtn.addEventListener("click", () => {
-        removeActiveClass()
+		removeActiveClass();
 		planningBtn.classList.toggle("active");
 		handleFilterClick("planning");
 	});
