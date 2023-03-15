30c6ce04697af5744ac94f94267b981f4bb1b0e2
Irina Kosogor <i.v.kosogor@gmail.com>
2023-03-15 08:58:25 +0200

    UPD blog news

--- JS/bundle.js
+++ JS/bundle.js
@@ -35,14 +35,16 @@ const blogNews = () => {
 		news?.articles.forEach((article) => {
 			const { urlToImage, title, description } = article;
 			const newsCard = document.createElement("div");
-			newsCard.classList.add("blog-news__item");
+			newsCard.classList.add("blog-news__item-wrapper");
 			newsCard.innerHTML = `
+				<div class="blog-news__item">
 				<div class="blog-news__item-img">
 					<img src="${urlToImage}" alt="blog-news-img">
 				</div>
 				<h3 class="blog-news__item-title">${title}</h3>
 				<div class="blog-news__item-description">${description}</div>
 				<button class="blog-news__item-btn">CONTINUE READING....</button>
+				</div>
 				`;
 			latestNewContainer.append(newsCard);
 		});
@@ -57,6 +59,37 @@ const blogNews = () => {
 	}
 
 	window.addEventListener("scroll", createCardsOnScroll);
+
+	// latestNewContainer.addEventListener("mouseover", (event) => {
+	// 	if (event.target.classList.contains("blog-news__item") || event.target.parentNode()) {
+	// 		// Scale up the image
+	// 		const image = event.target.querySelector(".blog-news__item-img img");
+	// 		image.style.transform = "scale(1.2)";
+	
+	// 		// Extend the description
+	// 		const description = event.target.querySelector(
+	// 			".blog-news__item-description"
+	// 		);
+	// 		description.classList.add("blog-news__item-description_extended");
+	// 	}
+	// });
+	
+	// // Add an event listener for mouseleave events on the parent element
+	// latestNewContainer.addEventListener("mouseout", (event) => {
+	// 	// Check if the event target is a card element
+	// 	const isCard = event.target.classList.contains("blog-news__item");
+	// 	if (isCard) {
+	// 		event.target.style.boxShadow = "";
+	
+	// 		const image = event.target.querySelector(".blog-news__item-img");
+	// 		image.style.transform = "scale(0.8)";;
+	
+	// 		const description = event.target.querySelector(
+	// 			".blog-news__item-description"
+	// 		);
+	// 		description.classList.remove("blog-news__item-description_extended");
+	// 	}
+	// });
 };
 
 
